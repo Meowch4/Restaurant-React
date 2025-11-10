d0280446bc0a9ad5d7d783d0bece10cba86c0b92
Matcha <Matcha@SuperPlatypus.com>
2025-11-10 16:37:16 +0800

    实现了菜品管理界面的上架下架功能

--- src/FoodManageView.tsx
+++ src/FoodManageView.tsx
@@ -13,6 +13,9 @@ class FoodManager {
   addFood(...food: Food[]) {
     this.foods.push(...food)
   }
+  setFoodStatus(idx:number, status: 'on' | 'off') {
+    this.foods[idx].status = status
+  }
 }
 function FoodManageView() {
   const [manager] = useState(() => observable(new FoodManager()))
@@ -30,13 +33,26 @@ function FoodManageView() {
     }
   }, [])
 
+  async function offShelf(idx: number) {
+    const food = manager.foods[idx]
+    await axios.put(`/api/restaurant/1/food/${food.id}`,
+      {status: 'off'})
+    manager.setFoodStatus(idx, 'off')
+  }
+  async function onShelf(idx: number) {
+    const food = manager.foods[idx]
+    await axios.put(`/api/restaurant/1/food/${food.id}`,
+      {status: 'on'})
+    manager.setFoodStatus(idx, 'on')
+  }
+
   return (
     <div>
       <span className="font-bold text-2xl">
         菜品管理
       </span>
       <Link to="/home/add-food" className="mx-4">添加菜品</Link>
-      {manager.foods.map(food => {
+      {manager.foods.map((food, idx) => {
         return (
           <li key={food.id} className="">
             <div className="border m-2 p-1 flex rounded">
@@ -48,7 +64,8 @@ function FoodManageView() {
                 <div className="p-1">分类：{ food.category }</div>
                 <div className="p-1">上架状态：{ food.status == 'on' ? '上架中' : '未上架' }</div>
                 <div>
-                  <button>下架</button>
+                  {food.status == 'on' && <button onClick={() => offShelf(idx)}>下架</button>}
+                  {food.status == 'off' && <button onClick={() => onShelf(idx)}>上架</button>}
                   <button>编辑</button>
                   <button>删除</button>
                 </div>
