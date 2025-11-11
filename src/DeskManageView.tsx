7add301f35b874de44793cc4eb2f3889ef3df044
Matcha <Matcha@SuperPlatypus.com>
2025-11-11 17:30:36 +0800

    实现了餐桌管理页面的编辑功能和删除功能

--- src/DeskManageView.tsx
+++ src/DeskManageView.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router"
 import QRCode from 'qrcode'
 import { makeAutoObservable } from "mobx"
 import { observer } from "mobx-react"
+import { useInput } from "./hooks"
 
 
 
@@ -20,8 +21,8 @@ class DeskManager {
   constructor() {
     makeAutoObservable(this)
   }
-  addDesk(...desk: Desk[]) {
-    this.desks.push(...desk)
+  addDesks( ...desks: Desk[]) {
+    this.desks.push(...desks)
   }
   deleteDesk(idx: number) {
     this.desks.splice(idx, 1)
@@ -31,9 +32,9 @@ class DeskManager {
 
 function DeskManageView() {
   const [manager] = useState(new DeskManager())
-  const [ignore, setIgnore] = useState(false)
   const [qrcodes, setQrcodes] = useState<string[]>([])
 
+  // 处理二维码url
   useEffect(() => {
     Promise.all(
       // 把每张桌子都映射为对应的地址创建的url
@@ -48,19 +49,16 @@ function DeskManageView() {
   }, [manager.desks.length])
 
   useEffect(() => {
+    let ignore = false
     axios.get(`/api/restaurant/1/desk`)
     .then(res => {
-      if (ignore == false) {
-        manager.addDesk(...res.data)
+      if (!ignore) {
+        manager.addDesks(...res.data)
       }
-      setIgnore(true)
     })
-  }, [ignore, manager])
-
-  function deleteDesk(desk: Desk, idx:number) {
-    axios.delete(`/api/restuarant/1/desk/${desk.id}`)
-    manager.deleteDesk(idx)
-  }
+    return () => {ignore = true}
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
 
   return (
@@ -71,22 +69,7 @@ function DeskManageView() {
       <Link to={'/home/add-desk'} className="mx-4">添加餐桌</Link>
       {
         manager.desks.map((desk, idx) => {
-          return (
-            <div key={ desk.id } className="border rounded flex p-2 m-2">
-              <div className="border-r p-2">
-                <div className="text-lg">名称：{ desk.name }</div>
-                <div className="text-lg">人数：{ desk.capacity }</div>
-                <div className="flex gap-2">
-                  <button>编辑</button>
-                  <button onClick={() => deleteDesk(desk, idx)}>删除</button>
-                  <button>打印二维码</button>
-                </div>
-              </div>
-              <div className="p-2">
-                <img data-url={`https://10.3.3.3:5173/r/1/d/${desk.id}`} src={qrcodes[idx]} alt="" className="w-24 h-24" />
-              </div>
-            </div>
-          )
+          return <DeskItem key={desk.id} manager={manager} desk={desk} idx={idx} qrcodes={qrcodes}/>
         })
       }
     </div>
@@ -95,3 +78,68 @@ function DeskManageView() {
 
 const DeskManageViewObserver = observer(DeskManageView)
 export default DeskManageViewObserver
+
+type DeskItemProps = {
+  manager: DeskManager,
+  desk: Desk,
+  idx: number,
+  qrcodes: string[],
+}
+
+const DeskItem: React.FC<DeskItemProps> = observer(( {manager, desk, idx, qrcodes} ) => {
+  const [editing, setEditing] = useState(false)
+
+  const name = useInput(desk.name)
+  const capacity = useInput(String(desk.capacity))
+
+  function editHandler() {
+    axios.put(`/api/restaurant/1/desk/${desk.id}`, {
+      name: name.value,
+      capacity: capacity.value,
+    }).then(res => {
+      manager.desks[idx] = res.data
+    })
+    setEditing(false)
+  }
+
+  if (editing == true) {
+    return (
+    <div className="border rounded flex p-2 m-2">
+      <div className="border-r p-2">
+        <div className="text-lg">名称：
+          <input className="rounded p-1 m-1" type="text" {...name} />
+        </div>
+        <div className="text-lg">人数：
+          <input className="rounded p-1 m-1" type="text" {...capacity}/>
+        </div>
+        <div className="flex gap-2">
+          <button onClick={editHandler}>完成</button>
+          <button onClick={() => setEditing(false)}>取消</button>
+        </div>
+      </div>
+    </div>
+    )
+  }
+
+  function deleteDesk(desk: Desk, idx:number) {
+    axios.delete(`/api/restaurant/1/desk/${desk.id}`)
+    manager.deleteDesk(idx)
+  }
+
+  return (
+    <div className="border rounded flex p-2 m-2">
+      <div className="border-r p-2">
+        <div className="text-lg">名称：{ desk.name }</div>
+        <div className="text-lg">人数：{ desk.capacity }</div>
+        <div className="flex gap-2">
+          <button onClick={() => setEditing(true)}>编辑</button>
+          <button onClick={() => deleteDesk(desk, idx)}>删除</button>
+          <button>打印二维码</button>
+        </div>
+      </div>
+      <div className="p-2">
+        <img data-url={`https://10.3.3.3:5173/r/1/d/${desk.id}`} src={qrcodes[idx]} alt="" className="w-24 h-24" />
+      </div>
+    </div>
+  )
+})
