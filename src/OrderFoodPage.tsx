1a66f63715cb4117994b0e10dc9e2ce3836852be
Matcha <Matcha@SuperPlatypus.com>
2025-11-12 20:46:10 +0800

    增加了点餐购物车功能

--- src/OrderFoodPage.tsx
+++ src/OrderFoodPage.tsx
@@ -1,11 +1,11 @@
-import { useRequest } from "ahooks"
+import { useRequest, useToggle } from "ahooks"
 import axios from "axios"
 import { useParams } from "react-router"
 import type { Food } from "./OrderManageView"
 import { useEffect, useState } from "react"
 import { useImmer } from "use-immer"
 
-function getMenu(rId: number | string) {
+function getMenu(rId: number | string): Promise<Food[]> {
   return axios.get('/api/menu/restaurant/' + rId)
   .then(res => {
     return res.data
@@ -16,6 +16,8 @@ function getMenu(rId: number | string) {
 function OrderFoodPage() {
   const params = useParams()
   const [foodCount, updateFoodCount] = useImmer<number[]>([])
+
+  const [expand, {toggle}] = useToggle(true)
 
   // data: menu是把data解构后别名menu
   const { data: menu, loading } = useRequest(getMenu, {
@@ -31,6 +33,13 @@ function OrderFoodPage() {
     })
   }
 
+  function totalPrice() {
+    return menu!.map(it => it.price)
+      .map((price: number, idx: number) => {
+        return price * foodCount[idx]
+      }).reduce((a, b) => a + b, 0)
+  }
+
 
 //   ### POST /restaurant/:rid/desk/:did/order
 
@@ -42,28 +51,42 @@ function OrderFoodPage() {
 //   foods: [{id, amount}, {}, {}]
 // }
 // ```
-  // function placeOrder() {
-  //   axios.post(`/api/restaurant/1/desk/2/order`, {
-  //     deskName: params.deskId,
-  //     customCount: ,
-  //     totalPrice: ,
-  //     foods: [
+  function placeOrder() {
+    axios.post(`/api/restaurant/1/desk/2/order`, {
+      // deskName: params.deskId,
+      // customCount: ,
+      // totalPrice: ,
+      // foods: [
 
-  //     ],
-  //   })
-  // }
+      // ],
+    })
+  }
 
   if (loading) {
     return 'Loading...'
   }
 
+  function selectedFood() {
+    return foodCount.map((count, idx) => {
+      return {
+        idx,
+        count,
+        food: menu![idx],
+      }
+    }).filter(it => it.count > 0)
+  }
+
+  function selectedFoodSum() {
+    return selectedFood().map(it => it.count).reduce((a, b) => a + b, 0)
+  }
+
   return (
     <div>
       <h1>点餐页面</h1>
-      <div>
+      <div className="pb-16">
         {
-          menu.map((foodItem: Food, idx: number) => (
-              <div className="border flex" key={idx}>
+          menu!.map((foodItem: Food, idx: number) => (
+              <div className="border flex p-2 m-2 rounded" key={idx}>
                 <img 
                 className="w-24 h-24"
                 src={`/upload/${foodItem.img}`} alt="" />
@@ -77,7 +100,41 @@ function OrderFoodPage() {
           ))
         }
       </div>
-      <button onClick={placeOrder}>下单</button>
+
+      <div className="fixed bottom-0 bg-slate-700 w-full ">
+        <div data-detail="当前购物车详情" hidden={expand}>
+          <div className="flex">
+            <div className="grow basis-0">菜品</div>
+            <div className="grow basis-0">数量</div>
+            <div className="grow basis-0">小计</div>
+          </div>
+          {
+            selectedFood()
+            .map(entry => {
+              return (
+                <div key={entry.idx} className="flex">
+                  <div className="grow basis-0">{ entry.food.name }</div>
+                  <div className="grow basis-0">{ entry.count }</div>
+                  <div className="grow basis-0">{ entry.count * entry.food.price }</div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="relative" onClick={toggle}>
+            展开
+            <span 
+            hidden={selectedFoodSum() == 0} 
+            className="absolute -right-1 -top-1 bg-red-500 text-white rounded-full text-sm w-6 h-6 flex items-center justify-center">
+              { selectedFoodSum() }
+            </span>
+          </button>
+          <span>￥ { totalPrice() }</span>
+          <button onClick={placeOrder}>下单</button>
+        </div>
+
+      </div>
     </div>
   )
 }
