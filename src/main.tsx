1cbecc62a5cb770dd22c1fdf3767f3a3608b1ee9
Matcha <Matcha@SuperPlatypus.com>
2025-11-15 16:01:54 +0800

    修改入口文件使antd mobile能兼容react19

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,24 @@ configure({
   enforceActions: 'never',
 })
 
+import 'antd-mobile/es/global'
+
+// @ts-ingnore
+import { unstableSetRender } from 'antd-mobile'; // Support since version ^5.40.0
+
+unstableSetRender((node, container) => {
+  const c = container as HTMLElement & { _reactRoot?: ReturnType<typeof createRoot> }
+  c._reactRoot ||= createRoot(c)
+  const root = c._reactRoot
+
+  root.render(node)
+
+  return async () => {
+    await new Promise(resolve => setTimeout(resolve, 0))
+    root.unmount()
+  }
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
