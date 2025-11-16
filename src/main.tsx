d0c35acda2c4dd84c0731c16ec01f242beeca841
Matcha <Matcha@SuperPlatypus.com>
2025-11-16 17:30:16 +0800

    使用ConfigProvider来应用antd的深色模式（真麻烦，不如antd-mobile））

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from 'react'
+import { StrictMode, useMemo } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {
-  RouterProvider,
-} from "react-router";
+import { RouterProvider } from "react-router";
 import router from './router.tsx'
 
 import { configure } from 'mobx'
@@ -13,8 +11,11 @@ configure({
 
 import 'antd-mobile/es/global'
 
+import { ConfigProvider, theme } from "antd";
+
 // @ts-ingnore
 import { unstableSetRender } from 'antd-mobile'; // Support since version ^5.40.0
+import { useDarkMode } from './hooks.tsx';
 
 unstableSetRender((node, container) => {
   const c = container as HTMLElement & { _reactRoot?: ReturnType<typeof createRoot> }
@@ -29,9 +30,23 @@ unstableSetRender((node, container) => {
   }
 });
 
+// eslint-disable-next-line react-refresh/only-export-components
+function Root() {
+  const [isDark] = useDarkMode()
+  const configTheme = useMemo(() => {
+      return { algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm}
+  }, [isDark])
+
+  return (
+  <ConfigProvider theme={configTheme}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </ConfigProvider>
+  )
+}
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Root />
 )
